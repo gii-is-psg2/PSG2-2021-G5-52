406ffe056bc32ab6809e174ffe5255926ba6a690
DJMori <alemorgar4@alum.us.es>
2021-04-16 13:42:31 +0200

    Task A3.2.1 Añadir entidad Donation aislada

---
+++ src/main/java/org/springframework/samples/petclinic/model/Donation.java
@@ -0,0 +1,41 @@
+package org.springframework.samples.petclinic.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "donations")
+public class Donation extends BaseEntity {
+
+		@NotNull
+		@Column(name = "amount", precision = 10 , scale = 2)
+		@Positive
+		private Double amount;
+		
+		@NotNull
+		@Column(name = "date")
+		@DateTimeFormat(pattern = "yyyy/MM/dd")
+		private Date date;
+
+//		@NotBlank
+//		@ManyToOne
+//		@JoinColumn(name = "causa_id")
+//		private Cause cause;
+		
+		@NotBlank
+		@ManyToOne
+		@JoinColumn(name = "owner_id")
+		private Owner owner;
+
+}
+
